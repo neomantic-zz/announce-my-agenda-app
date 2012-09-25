b8645b14be39d12929052986f93716ac76b03e43
Chad Albers <calbers@neomantic.com>
2012-09-24 21:39:48 -0400

    added agenda line class and new lines for formatting

--- gen/com/neomantic/calendar_out_loud/R.java
+++ gen/com/neomantic/calendar_out_loud/R.java
@@ -31,10 +31,21 @@ public final class R {
         public static final int activity_main=0x7f070000;
     }
     public static final class string {
+        public static final int agenda_line_item=0x7f050006;
+        public static final int agenda_line_item_final_meeting=0x7f05000a;
+        public static final int agenda_line_item_final_meeting_no_location=0x7f05000b;
+        public static final int agenda_line_item_next_meeting=0x7f050008;
+        public static final int agenda_line_item_next_meeting_no_location=0x7f050009;
+        public static final int agenda_line_item_no_location=0x7f050007;
+        public static final int agenda_line_one_meeting=0x7f05000d;
+        public static final int agenda_line_one_meeting_no_location=0x7f05000e;
+        public static final int agenda_no_meeting=0x7f05000c;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int title_activity_main=0x7f050003;
+        public static final int first_agenda_line_item=0x7f050004;
+        public static final int first_agenda_line_item_no_location=0x7f050005;
+        public static final int menu_settings=0x7f050001;
+        public static final int morning_greeting=0x7f050003;
+        public static final int title_activity_main=0x7f050002;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
---
+++ src/com/neomantic/calendar_out_loud/AgendaLine.java
@@ -0,0 +1,164 @@
+package com.neomantic.calendar_out_loud;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Random;
+import java.util.TimeZone;
+
+import android.content.res.Resources;
+import android.database.Cursor;
+
+public class AgendaLine {
+	
+	private String mEventLocation = "";
+	private String mEventTitle = "";
+	private String mBeginTime = "";
+	private String mEndTime = "";
+	
+	public static final int FIRST_MEETING = R.string.first_agenda_line_item;
+	public static final int FIRST_MEETING_NO_LOCATION = R.string.first_agenda_line_item_no_location;
+		
+	public static final int MEETING = R.string.agenda_line_item;
+	public static final int MEETING_NO_LOCATION = R.string.agenda_line_item_no_location;
+
+	public static final int ONLY_ONE_MEETING = R.string.agenda_line_one_meeting;
+	public static final int ONLY_ONE_MEETING_NO_LOCATION = R.string.agenda_line_one_meeting_no_location;
+	
+	public static final int NEXT_MEETING = R.string.agenda_line_item_next_meeting;
+	public static final int NEXT_MEETING_NO_LOCATION = R.string.agenda_line_item_next_meeting_no_location;
+	
+	public static final int LAST_MEETING = R.string.agenda_line_item_final_meeting;
+	public static final int LAST_MEETING_NO_LOCATION = R.string.agenda_line_item_final_meeting_no_location;
+
+	private int[] DEFAULTS = new int[] {NEXT_MEETING, MEETING};
+	private int mFormatResourceId;
+	
+	public AgendaLine(Cursor cursor) {
+		
+		mEventTitle = cursor.getString(Agenda.INDEX_TITLE);
+		mEventLocation = cursor.getString(Agenda.INDEX_EVENT_LOCATION);
+		
+		GregorianCalendar cal = new GregorianCalendar(TimeZone.getDefault());
+		cal.setTimeInMillis(cursor.getLong(Agenda.INDEX_BEGIN));
+
+		
+		mBeginTime = Integer.toString(cal.get(Calendar.HOUR)) + " ";
+		mBeginTime += parseMinute(cal);
+		mBeginTime += parseAMPM(cal);
+
+		cal = new GregorianCalendar(TimeZone.getDefault());
+		cal.setTimeInMillis(cursor.getLong(Agenda.INDEX_END));
+		
+		mEndTime = Integer.toString(cal.get(Calendar.HOUR)) + " ";
+		mEndTime += parseMinute(cal);
+		mEndTime += parseAMPM(cal);
+		
+		Random r = new Random();
+		mFormatResourceId = DEFAULTS[r.nextInt(1)];
+	}
+
+	private String parseMinute(GregorianCalendar cal) {
+		String min = "";
+		int minute = cal.get(Calendar.MINUTE);
+		if (minute > 0) {
+			min = Integer.toString(minute);
+			min += " ";
+		}
+		return min;
+	}
+	
+	private String parseAMPM(GregorianCalendar cal) {
+		if (cal.get(Calendar.AM_PM) == Calendar.AM ) {
+			return "A M";
+		} else {	
+			return "P M";    	    			
+		}		
+	}
+
+	public String getEventLocation() {
+		return mEventLocation;
+	}
+
+	public String getEventTitle() {
+		return mEventTitle;
+	}
+
+	public String getBeginTime() {
+		return mBeginTime;
+	}
+
+	public String getEndTime() {
+		return mEndTime;
+	}
+
+	public void setFirst() {
+		if (hasLocation()) {
+			mFormatResourceId = FIRST_MEETING;
+		} else {
+			mFormatResourceId = FIRST_MEETING_NO_LOCATION;
+		}
+	}
+	
+	public void setLast() {
+		if (hasLocation()) {
+			mFormatResourceId = LAST_MEETING;
+		} else {
+			mFormatResourceId = LAST_MEETING_NO_LOCATION;
+		}
+	}
+
+	public void setOnlyOneMeeting() {
+		if (hasLocation()) {
+			mFormatResourceId= ONLY_ONE_MEETING;
+		} else {
+			mFormatResourceId= ONLY_ONE_MEETING_NO_LOCATION;
+		}
+	}
+	
+	private boolean hasLocation() {
+		/* NOTE - will be adding more logic here to find phone numbers etc */
+		return !mEventLocation.isEmpty();
+	}
+
+	public String format(Resources res) {
+		String[] eventArgs = null;
+		/* TODO - I hate handling it this way */
+		switch(mFormatResourceId) {
+		case FIRST_MEETING:
+			eventArgs = new String[]{mBeginTime, mEndTime, mEventTitle, mEventLocation};
+			break;
+		case FIRST_MEETING_NO_LOCATION:
+			eventArgs = new String[]{mBeginTime, mEndTime, mEventTitle};
+			break;
+		case MEETING:
+			eventArgs = new String[]{mBeginTime, mEndTime, mEventTitle, mEventLocation};
+			break;
+		case MEETING_NO_LOCATION:
+			eventArgs = new String[]{mBeginTime, mEndTime, mEventTitle};
+			break;
+		case ONLY_ONE_MEETING:
+			eventArgs = new String[]{mBeginTime, mEndTime, mEventTitle, mEventLocation};
+			break;
+		case ONLY_ONE_MEETING_NO_LOCATION:
+			eventArgs = new String[]{mBeginTime, mEndTime, mEventTitle};
+			break;
+		case NEXT_MEETING:
+			eventArgs = new String[]{mEventTitle, mBeginTime, mEndTime, mEventLocation};
+			break;
+		case NEXT_MEETING_NO_LOCATION:
+			eventArgs = new String[]{mEventTitle, mBeginTime, mEndTime};
+			break;
+		case LAST_MEETING:
+			eventArgs = new String[]{mBeginTime, mEndTime, mEventLocation, mEventTitle};
+			break;
+		case LAST_MEETING_NO_LOCATION:
+			eventArgs = new String[]{mBeginTime, mEndTime, mEventTitle};
+			break;
+		}
+		if (eventArgs == null) {
+			return "";
+		}
+		return String.format(res.getString(mFormatResourceId), (Object[])eventArgs);
+	}
+
+}
