d93e664856090930dfa8753809478ee3fa5f3637
Chad <calbers@neomantic.com>
2012-09-24 22:00:56 -0400

    added greetings

--- src/com/neomantic/calendar_out_loud/Script.java
+++ src/com/neomantic/calendar_out_loud/Script.java
@@ -1,6 +1,9 @@
 package com.neomantic.calendar_out_loud;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 import android.content.res.Resources;
 
@@ -13,12 +16,12 @@ public class Script {
 	}
 	
 	public String write(Resources res) {
+		String script = res.getString(getGreetingResourceId()) + " ";		
 		final int numberOfItems = mAgendaItems.size();
 		if (numberOfItems == 0) {
-			return res.getString(R.string.agenda_no_meeting);
+			return script + res.getString(R.string.agenda_no_meeting);
 		}
-		String script = "";
-		
+
 		for (int i = 0; i < numberOfItems; i++) {
 			AgendaLine line = mAgendaItems.get(i);
 			if (i == 0 && numberOfItems == 1) {
@@ -30,8 +33,19 @@ public class Script {
 			} 
 			script += line.format(res);
 		}
-
 		return script;
 	}
 
+	private int getGreetingResourceId() {
+		final GregorianCalendar cal = new GregorianCalendar(TimeZone.getDefault());
+		if (cal.get(Calendar.AM_PM) == Calendar.AM) {
+			return R.string.greeting_good_morning; 
+		}
+		int hour = cal.get(Calendar.HOUR);
+		if (hour > 6) {
+			return R.string.greeting_evening;
+		}
+		
+		return R.string.greeting_afternoon;
+	}
 }
