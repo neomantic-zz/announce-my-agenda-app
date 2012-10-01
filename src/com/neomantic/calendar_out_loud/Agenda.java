b76452174573f392236ea61d93e7aa30a47acb03
Chad Albers <calbers@neomantic.com>
2012-10-01 11:18:54 -0400

    forgot set import

--- src/com/neomantic/calendar_out_loud/Agenda.java
+++ src/com/neomantic/calendar_out_loud/Agenda.java
@@ -2,6 +2,7 @@ package com.neomantic.calendar_out_loud;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Set;
 import java.util.TimeZone;
 
 import android.content.ContentResolver;
