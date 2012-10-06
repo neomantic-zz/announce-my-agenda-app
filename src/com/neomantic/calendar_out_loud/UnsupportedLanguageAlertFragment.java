b6ba9ada382982ef7dfc54f0f28a4f892a8906fc
Chad Albers <calbers@neomantic.com>
2012-10-06 15:03:30 -0400

    added alert box when the user's TTS locale is non-English.  And disabled app interactivity.

---
+++ src/com/neomantic/calendar_out_loud/UnsupportedLanguageAlertFragment.java
@@ -0,0 +1,33 @@
+package com.neomantic.calendar_out_loud;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class UnsupportedLanguageAlertFragment extends DialogFragment {
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		return new AlertDialog.Builder(getActivity())
+			.setIcon(android.R.drawable.ic_dialog_alert)
+			.setTitle(R.string.alert_title_unsupported_language)
+			.setMessage(R.string.alert_btn_unsupported_language_msg)
+			.setPositiveButton(R.string.alert_btn_unsupported_language_ok, 
+					new DialogInterface.OnClickListener() {
+						
+						public void onClick(DialogInterface dialog, int which) {
+							MainActivity m = ((MainActivity) getActivity());
+							m.disableSpeechButton();
+							m.disableListView();
+						}
+					})
+			.create();
+	}
+
+	public static DialogFragment newInstance() {
+		return new UnsupportedLanguageAlertFragment();
+	}
+
+}
